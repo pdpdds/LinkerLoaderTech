0cd63df39e63e64f899d202efebbe39746ea96ad
Juhang Park <juhang3@daum.net>
2023-01-08 15:57:14 +0900

    build test

--- src/stack/stack_sample.s
+++ src/stack/stack_sample.s
@@ -1,42 +1,59 @@
 	.file	"stack_sample.c"
-	.version	"01.01"
-gcc2_compiled.:
-.text
-	.p2align 2,0x90
-.globl func
-		.type		 func,@function
+	.text
+	.globl	func
+	.type	func, @function
 func:
-	pushl %ebp
-	movl %esp,%ebp
-	xorl %eax,%eax
-	jmp .L6
-	.p2align 2,0x90
-.L6:
-	leave
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
 	ret
-.Lfe1:
-		.size		 func,.Lfe1-func
-	.p2align 2,0x90
-.globl main
-		.type		 main,@function
+	.cfi_endproc
+.LFE0:
+	.size	func, .-func
+	.globl	main
+	.type	main, @function
 main:
-	pushl %ebp
-	movl %esp,%ebp
-	subl $8,%esp
-	addl $-12,%esp
-	addl $-8,%esp
-	pushl $2
-	pushl $1
-	call func
-	addl $16,%esp
-	movl %eax,%eax
-	pushl %eax
-	call exit
-	addl $16,%esp
-	.p2align 2,0x90
-.L7:
-	leave
-	ret
-.Lfe2:
-		.size		 main,.Lfe2-main
-	.ident	"GCC: (GNU) c 2.95.4 20020320 [FreeBSD]"
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$2, %esi
+	movl	$1, %edi
+	call	func
+	movl	%eax, %edi
+	call	exit@PLT
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
